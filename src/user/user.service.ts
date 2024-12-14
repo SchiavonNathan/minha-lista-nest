d0bc613e3718eab5a13327a319eceed7b64a123e
SchiavonNathan <Nathanmestrepk@gmail.com>
2024-12-14 09:37:14 -0300

    Funcao de criacao de usuario (UserService e UserController)  e implementacao final do Prisma

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { userDTO } from './DTO/user.dto';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    constructor(private prisma: PrismaService) {}
+
+
+    async create(data: userDTO) {
+        
+        const userExists = await this.prisma.user.findFirst({
+            where:{
+                email: data.email
+            }
+        });
+
+        if (userExists) {
+            throw new Error('O Email já está sendo usado')
+        }
+        
+        const user = await this.prisma.user.create({
+            data,
+        })
+
+        return user
+    }
+
+}
